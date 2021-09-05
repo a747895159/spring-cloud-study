47d9a11f493dcffa52f42a21ad4731fe5620af76
zhoubin <---->
2021-09-06 00:24:37 +0800

    项目初始化

---
+++ study-atomic-common/src/main/java/com/person/zb/study/common/A.java
@@ -0,0 +1,9 @@
+package com.person.zb.study.common;
+
+/**
+ * @Desc:
+ * @Author: ZhouBin
+ * @Date: 2021/9/6
+ */
+public class A {
+}
---
+++ study-atomic-util/src/main/java/com/person/zb/study/base/A.java
@@ -0,0 +1,9 @@
+package com.person.zb.study.base;
+
+/**
+ * @Desc:
+ * @Author: ZhouBin
+ * @Date: 2021/9/6
+ */
+public class A {
+}
