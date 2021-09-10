fa76cd6c3bbe5de9bb64b0a5c8216012878f55a7
zhoubin <zhoubin2@yonghui.cn>
2021-09-10 10:42:11 +0800

    增加Eureka 配置

--- eureka-server/src/main/java/com/person/zb/eureka/EurekaBootstrap.java
+++ eureka-server/src/main/java/com/person/zb/eureka/EurekaBootstrap.java
@@ -1,12 +1,14 @@
 package com.person.zb.eureka;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Desc:
  * @Author: ZhouBin
  * @Date: 2021/9/5
  */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaBootstrap {
 
